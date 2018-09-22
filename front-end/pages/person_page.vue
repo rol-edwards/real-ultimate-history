a38aca08f49a833f20ac6380e1d8b02b37723bfc
Roland Edwards <rol.edwards@live.com>
2018-09-22 21:50:48 +0100

    person page now only shows key events table if there are some

--- front-end/pages/person_page.vue
+++ front-end/pages/person_page.vue
@@ -9,10 +9,11 @@
 						<img class='bin' v-on:click='delete_person' v-bind:src="'../images/bin.png'">
 					
 						<img class='bin' v-on:click='delete_person' v-bind:src="'../images/pen.png'">
-					
-
 				</h1>
 			</div>
+			<figure>
+				<img v-bind:src="'../images/' + person.id + '.jpg'">
+			</figure>
 			<table class='info'>
 				<tr>
 					<td>Lived:</td>
@@ -28,28 +29,27 @@
 				</tr>
 			</table>
 
-			<figure>
-				<img v-bind:src="'../images/' + person.id + '.jpg'">
-			</figure>
+			
 			
 			<h2>Biography</h2>
 			<p>{{ person.bio }}</p>
-			
-			<h2>Key Events</h2>
-			<table class='table'>
-				<tr>
-					<th style='text-align:left'>Event</th>
-					<th style='text-align:left'>Date</th>
-					<th style='text-align:left'>Role</th>
-				</tr>
-				<tr v-for='event in person_event'>
-					<td><a v-bind:href="'/#/event/' + event.id">{{event.name}}</a></td>
-					<td>{{event.date}}</td>
-					<td>{{event.role}}</td>
-				</tr>
-			</table>
-			<button v-show='editable'><a v-bind:href='"/#/edit_person/" + id'>Edit</a></button>
-			<p v-show='editable'><button v-on:click='delete_person'>Delete</button></p>
+			<div v-if='show_events'>
+				<h2>Key Events</h2>
+				<table class='table'>
+					<tr>
+						<th style='text-align:left'>Event</th>
+						<th style='text-align:left'>Date</th>
+						<th style='text-align:left'>Role</th>
+					</tr>
+					<tr v-for='event in person_event'>
+						<td><a v-bind:href="'/#/event/' + event.id">{{event.name}}</a></td>
+						<td>{{event.date}}</td>
+						<td>{{event.role}}</td>
+					</tr>
+				</table>
+				<button v-show='editable'><a v-bind:href='"/#/edit_person/" + id'>Edit</a></button>
+				<p v-show='editable'><button v-on:click='delete_person'>Delete</button></p>
+			</div>
 		</div>
 		<div v-show='person_deleted'>
 			<h1>Person deleted</h1>
@@ -67,6 +67,7 @@ module.exports = {
 		return{
 			person: 'placeholder',
 			person_event: 'placeholder',
+			show_events: false,
 			id: this.$route.params.id,
 			editable: false,
 			deleted_data: '',
@@ -109,6 +110,9 @@ module.exports = {
 				.then( function(data) {
 					console.log('events for person: ' + data);
 					this.person_event = data.body;
+					if (this.person_event.length > 0){
+						this.show_events = true;
+					}
 					numberToDate = this.numberToDate;
 					this.person_event.forEach(function(event){
 					event.date = this.numberToDate(event.date);
