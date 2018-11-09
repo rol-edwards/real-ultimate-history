6b09a62e0e26af925bf6c1f1dcbaa653137d88ea
Roland Edwards <rol.edwards@live.com>
2018-11-09 20:09:50 +0000

    server.js ammended to get port from config file.

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,10 @@
 // Import express app. 
 var app = require(__dirname + '/app.js'); 
+var config = require(__dirname + '/config.js')
 
 
 // Set port. 
-var port = process.env.PORT || 3000; 
-
+var port = process.env.PORT || config.port; 
 // Start server. 
 app.listen(port);
 console.log("server running on port 3000");  
